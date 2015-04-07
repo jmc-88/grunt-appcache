ab7cb6f0d8fad3b039f6df55c3ec3c1dc58983d6
Daniele Cocca <daniele.cocca@gmail.com>
2015-04-08 01:17:22 +0200

    The named functions weren't really necessary.

--- tasks/lib/utils.js
+++ tasks/lib/utils.js
@@ -12,40 +12,36 @@ module.exports.init = function (grunt) {
   var path = require('path');
   var exports = {};
 
-  function _array(input) {
+  exports.array = function (input) {
     if (arguments.length == 1 && Array.isArray(input)) {
       return input;
     }
     return Array.apply(undefined, arguments);
-  }
-  exports.array = _array;
+  };
 
-  function _isUrl(path) {
+  exports.isUrl = function (path) {
     return (/^(?:https?:)?\/\//i).test(path);
-  }
-  exports.isUrl = _isUrl;
+  };
 
-  function _joinUrl( /* ... */ ) {
+  exports.joinUrl = function ( /* ... */ ) {
     return Array.prototype.map.call(arguments, function (part) {
       // remove trailing slashes
       return part.replace(/\/+$/, '');
     }).join('/');
-  }
-  exports.joinUrl = _joinUrl;
+  };
 
-  function _relative(basePath, filePath) {
+  exports.relative = function (basePath, filePath) {
     return path.relative(
       path.normalize(basePath),
       path.normalize(filePath));
-  }
-  exports.relative = _relative;
+  };
 
-  function _expand(patterns, basePath) {
+  exports.expand = function (patterns, basePath) {
     var urls = [];
     var globs = [];
 
-    _array(patterns).forEach(function (pattern) {
-      if (_isUrl(pattern)) {
+    exports.array(patterns).forEach(function (pattern) {
+      if (exports.isUrl(pattern)) {
         urls.push(pattern);
       } else {
         globs.push(pattern);
@@ -60,20 +56,18 @@ module.exports.init = function (grunt) {
 
     if (typeof basePath === 'string') {
       matches = matches.map(function (filePath) {
-        return _relative(basePath, filePath);
+        return exports.relative(basePath, filePath);
       });
     }
 
     return matches.concat(urls);
-  }
-  exports.expand = _expand;
+  };
 
-  function _uniq(array) {
+  exports.uniq = function (array) {
     return array.filter(function (value, index) {
       return array.indexOf(value) === index;
     });
-  }
-  exports.uniq = _uniq;
+  };
 
   return exports;
 };
